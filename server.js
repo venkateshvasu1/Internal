ff2b620bc2e863164cbdcdc1850497f132fe3316
venkateshvasu1 <venkatesh.vasu1987@gmail.com>
2020-04-02 18:49:56 +0530

    Firestore addition

--- server.js
+++ server.js
@@ -16,21 +16,49 @@ app.use(bodyParser.json());
 
 // mock events data - for a real solution this data should be coming 
 // from a cloud data store
+const Firestore = require("@google-cloud/firestore");
+
+const firestore = new Firestore(
+    {
+        projectId: process.env.GOOGLE_CLOUD_PROJECT
+    }
+);
+
 const mockEvents = {
     events: [
-        { title: 'an event', id: 1, description: 'something really cool' },
+        { title: 'an event', id: 1, description: 'something really cool', location:'Bangalore' },
         { title: 'another event', id: 2, description: 'something even cooler' },
         { title: 'another event', id: 3, description: 'something even cooler' }
     ]
 };
 
-
-
-
 // health endpoint - returns an empty array
 app.get('/', (req, res) => {
     res.json([]);
 });
+
+// Event to connect to firestore
+
+function getEvents(req, res) {
+    firestore.collection("Events").get()
+        .then((snapshot) => {
+            if (!snapshot.empty) {
+                const ret = { events: []};
+                snapshot.docs.forEach(element => {
+                    ret.events.push(element.data());
+                }, this);
+                console.log(ret);
+                res.json(ret);
+            } else {
+                res.json(mockEvents);
+            }
+        })
+        .catch((err) => {
+            console.error('Error getting events', err);
+            res.json(mockEvents);
+        });
+};
+
 
 // version endpoint to provide easy convient method to demonstrating tests pass/fail
 app.get('/version', (req, res) => {
@@ -41,8 +69,9 @@ app.get('/version', (req, res) => {
 // mock events endpoint. this would be replaced by a call to a datastore
 // if you went on to develop this as a real application.
 app.get('/events', (req, res) => {
-    res.json(mockEvents);
+    getEvents(req, res);
 });
+
 
 // Adds an event - in a real solution, this would insert into a cloud datastore.
 // Currently this simply adds an event to the mock array in memory
@@ -55,11 +84,16 @@ app.post('/event', (req, res) => {
         location: req.body.location,
         id : mockEvents.events.length + 1
      }
-    // add to the mock array
-    mockEvents.events.push(ev);
-    // return the complete array
-    res.json(mockEvents);
+    firestore.collection("Events").add(ev).then(ret => {
+        getEvents(req, res);
+    });
+
+    // // add to the mock array
+    // mockEvents.events.push(ev);
+    // // return the complete array
+    // res.json(mockEvents);
 });
+
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
